6da525a5bb1834f23b225e3bf047ff151be4743c
Denis Berestinskii <berestinsky@gmail.com>
2023-12-26 02:20:03 +0200

    common module

---
+++ loomoroutines-common/src/main/java/dev/reformator/loomoroutines/common/Dummy.java
@@ -0,0 +1,2 @@
+package dev.reformator.loomoroutines.common;
+class Dummy { }
---
+++ loomoroutines-common/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module dev.reformator.loomoroutines.common {
+    requires static kotlin.stdlib;
+
+    exports dev.reformator.loomoroutines.common;
+}
